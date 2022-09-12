0eac8a2695800d46a66d69b29766d6ece31b05bd
Idelvan <delvanpj@gmail.com>
2022-09-12 19:09:37 -0300

    Testando jenkis novo

--- src/main/java/com/idsystem/api/master/SistemaControleVendasApplication.java
+++ src/main/java/com/idsystem/api/master/SistemaControleVendasApplication.java
@@ -10,6 +10,7 @@ public class SistemaControleVendasApplication {
 		SpringApplication.run(SistemaControleVendasApplication.class, args);
 		
 		System.out.println("Ola mundo");
+		System.out.println("Ola Brasil");
 	}
 
 }
